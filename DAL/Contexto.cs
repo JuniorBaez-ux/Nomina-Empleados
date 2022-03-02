a330e47fed467b323e63ccbfa2b17c6e1f31c17e
JuniorBaez-ux <junior_baez@ucne.edu.do>
2022-03-02 08:53:13 -0400

    Creando el contexto del proyecto.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nomina_Empleados_Leng.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nomina_Empleados_Leng.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Empleados> Empleados { get; set; }
+        public DbSet<Nominas> Nominas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\EmpleadoNomina.db");
+        }
+    }
+}
