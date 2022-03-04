fb325310b6dd2847a4614ef38ad6f8dad4d8aad9
JuniorBaez-ux <junior_baez@ucne.edu.do>
2022-03-03 21:43:20 -0400

    Implementando la logica del registro de nominas,

---
+++ UI/Registros/rNominas.xaml.cs
@@ -0,0 +1,215 @@
+﻿using Nomina_Empleados_Leng.BLL;
+using Nomina_Empleados_Leng.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Nomina_Empleados_Leng.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for rNominas.xaml
+    /// </summary>
+    public partial class rNominas : Window
+    {
+        private Nominas nominas = new Nominas();
+        public rNominas()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            this.DataContext = nominas;
+            //——————————————————————————[ VALORES DEL ComboBox Empleado Id]——————————————————————————
+            EmpleadoComboBox.ItemsSource = EmpleadosBLL.GetEmpleados();
+            EmpleadoComboBox.SelectedValuePath = "EmpleadoId";
+            EmpleadoComboBox.DisplayMemberPath = "NombreCompleto";
+        }
+
+        private void BuscarIDButton_Click(object sender, RoutedEventArgs e)
+        {
+            Nominas encontrado = NominasBLL.Buscar(Utilidades.ToInt(IdTextBox.Text));
+
+            if (encontrado != null)
+            {
+                this.DataContext = null;
+                this.DataContext = encontrado;
+                //Cargar();
+            }
+            else
+            {
+                this.nominas = new Nominas();
+                this.DataContext = this.nominas;
+
+                MessageBox.Show($"Este Contacto no fue encontrado.\n\nAsegúrese que existe o cree uno nuevo.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+                Limpiar();
+                IdTextBox.SelectAll();
+                IdTextBox.Focus();
+            }
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                if (!Validar())
+                    return;
+
+                //———————————————————————————————————————————————————————[ VALIDAR SI ESTA VACIO ]———————————————————————————————————————————————————————
+                //—————————————————————————————————[ Nomina Id ]—————————————————————————————————
+                if (IdTextBox.Text.Trim() == string.Empty)
+                {
+                    MessageBox.Show("El Campo (Contacto Id) está vacío.\n\nPorfavor, Asigne un Id al Contacto.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    IdTextBox.Text = "0";
+                    IdTextBox.Focus();
+                    IdTextBox.SelectAll();
+                    return;
+                }
+
+
+                //———————————————————————————————————————————————————————[ VALIDAR SI ESTA VACIO - FIN ]———————————————————————————————————————————————————————
+                var paso = NominasBLL.Guardar(nominas);
+                if (paso)
+                {
+                    Limpiar();
+                    MessageBox.Show("El Registro se pudo guardar satisfactoriamente.", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                    MessageBox.Show("El Registro no se pudo guardar :(", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                if (NominasBLL.Eliminar(Utilidades.ToInt(IdTextBox.Text)))
+                {
+                    Limpiar();
+                    MessageBox.Show("Registro Eliminado", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                    MessageBox.Show("No se pudo eliminar el registro", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void NominasIdTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (IdTextBox.Text.Trim() != string.Empty)
+                {
+                    int.Parse(IdTextBox.Text);
+                }
+            }
+            catch
+            {
+                MessageBox.Show($"El valor digitado en el campo (Contacto Id) debe ser un número.\n\nPor favor, digite un número valido.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                IdTextBox.Text = "0";
+                IdTextBox.Focus();
+                IdTextBox.SelectAll();
+            }
+        }
+
+        private void SalarioMensualTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CalcularSalario();
+        }
+
+
+
+        private void CalcularSalario()
+        {
+            if (SalarioMensualTextBox.Text == "0")
+            {
+                HorasExtraTextBox.IsEnabled = false;
+            }
+            else
+            {
+                HorasExtraTextBox.IsEnabled = true;
+            }
+
+            nominas.SalarioMensual = double.Parse(SalarioMensualTextBox.Text.ToString());
+            nominas.SalarioMensual += nominas.HorasExtra * 200;
+
+
+
+
+            //—————————————[Formulas para Seguro Familiar de Salud o SFS]—————————————
+
+            double SFS = 0.0304;
+            //—————————————[Formulas para Administradora de Fondos de Pensiones o AFP]—————————————
+            double AFP = 0.0287;
+
+            //—————————————————————————————————————————[Total SFS y AFP]————————————————————————————————————————————
+            double T_SFS = (nominas.SalarioMensual * SFS);
+            double T_AFP = (nominas.SalarioMensual * AFP);
+
+
+            //—————————————[Formulas para Impuesto sobre renta o ISR]—————————————
+            //double DEDUC = T_SFS + T_AFP;
+            // double RDEDUC = SM - DEDUC;
+            // double IMPOx12 = RDEDUC * 12;
+            //double EXCEDENT = IMPOx12 - 100 ;
+            //double APLICA20 = EXCEDENT * 0.20;
+            //double PASO_ADICC = APLICA20 + 31216;
+
+            double T_ISR = nominas.SalarioMensual * 0.10;
+            //624329.01
+
+            //—————————————————————————————————————————[Total Descuentos]————————————————————————————————————————————
+            double T_Descuentos = (T_SFS + T_AFP + T_ISR);
+
+            SFSTextBox.Text = Convert.ToString(Math.Round(T_SFS, 2));
+            AFPTextBox.Text = Convert.ToString(Math.Round(T_AFP, 2));
+            ISRTextBox.Text = Convert.ToString(Math.Round(T_ISR, 2));
+
+            SueldoTotalTextBox.Text = Convert.ToString(Math.Round(nominas.SalarioMensual, 2));
+            TotalDecuentosTextBox.Text = Convert.ToString(Math.Round(nominas.SalarioMensual - T_Descuentos, 2));
+        }
+
+        private void HorasExtraTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int HorasExtra = Convert.ToInt32(HorasExtraTextBox.Text);
+            nominas.SalarioMensual += HorasExtra * 200;
+            SalarioMensualTextBox.Text = nominas.SalarioMensual.ToString();
+            CalcularSalario();
+
+        }
+
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = nominas;
+        }
+        //——————————————————————————————————————————————————————————————[ Limpiar ]——————————————————————————————————————————————————————————————
+        private void Limpiar()
+        {
+            this.nominas = new Nominas();
+            this.DataContext = nominas;
+        }
+        //——————————————————————————————————————————————————————————————[ Validar ]——————————————————————————————————————————————————————————————
+        private bool Validar()
+        {
+            bool Validado = true;
+            if (IdTextBox.Text.Length == 0)
+            {
+                Validado = false;
+                MessageBox.Show("Transacción Fallida\n\nNo se pudo validar.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            return Validado;
+        }
+    }
+}
